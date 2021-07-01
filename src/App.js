ce891523198fc47e8d251db3b5e5d3261947721d
enkoding1 <ksjdevelop53@gmail.com>
2021-07-01 16:59:03 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+
 
 function App() {
+  
+
+  let [inputVal1,inputCh1] =  useState();
+  let [inputVal2, inputCh2] = useState();
+
+
+
   return (
     <div className="App">
-       <h1>Hello world</h1>
+       <input type='number' onChange={(e)=>inputCh1(e.target.value)} value={inputVal1}/>
+       <input type='number' onChange={(e)=>inputCh2(e.target.value)} value={inputVal2} />
+       <button onClick={()=>{var sum = parseInt(inputVal1)+parseInt(inputVal2); alert(sum);}}>+</button>
+       <button onClick={()=>{var min = parseInt(inputVal1)-parseInt(inputVal2); alert(min);}}>-</button>
     </div>
   );
 }
